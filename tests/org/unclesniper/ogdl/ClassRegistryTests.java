12ea04b3045dddf23399e59ce2332af4d577cdc6
Simon "UncleSniper" Bausch <simon@bausch-alfdorf.de>
2014-04-30 11:32:58 +0200

    added ClassRegistryTests

---
+++ tests/org/unclesniper/ogdl/ClassRegistryTests.java
@@ -0,0 +1,22 @@
+package org.unclesniper.ogdl;
+
+import org.junit.Test;
+import static org.junit.Assert.assertSame;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+public class ClassRegistryTests {
+
+	private static class Empty {}
+
+	@Test
+	public void forClass() {
+		ClassRegistry reg = new ClassRegistry();
+		ClassInfo first = reg.forClass(Empty.class);
+		assertNotNull("created class info", first);
+		assertEquals("class info subject class", Empty.class, first.getSubject());
+		ClassInfo second = reg.forClass(Empty.class);
+		assertSame("repeated class info", first, second);
+	}
+
+}
