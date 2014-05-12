09e89aec3d39ca777f2efc0d1e4d7c7b5fc1e1f4
Simon "UncleSniper" Bausch <simon@bausch-alfdorf.de>
2014-05-12 17:51:27 +0200

    ClassStringClassMapper deserializes primitive types

--- src/org/unclesniper/ogdl/ClassStringClassMapper.java
+++ src/org/unclesniper/ogdl/ClassStringClassMapper.java
@@ -1,10 +1,28 @@
 package org.unclesniper.ogdl;
 
+import java.util.Map;
+import java.util.HashMap;
+
 public class ClassStringClassMapper implements StringClassMapper {
+
+	private static final Map<String, Class<?>> namedClasses;
+
+	static {
+		namedClasses = new HashMap<String, Class<?>>();
+		namedClasses.put("byte", Byte.TYPE);
+		namedClasses.put("char", Character.TYPE);
+		namedClasses.put("short", Integer.TYPE);
+		namedClasses.put("int", Integer.TYPE);
+		namedClasses.put("long", Long.TYPE);
+		namedClasses.put("float", Float.TYPE);
+		namedClasses.put("double", Double.TYPE);
+	}
 
 	public boolean canDeserializeObject(String specifier, Class<?> desiredType, ClassLoader loader) {
 		if(!Class.class.isAssignableFrom(desiredType))
 			return false;
+		if(ClassStringClassMapper.namedClasses.containsKey(specifier))
+			return true;
 		if(loader == null)
 			loader = ClassStringClassMapper.class.getClassLoader();
 		try {
@@ -17,6 +35,8 @@ public class ClassStringClassMapper implements StringClassMapper {
 	}
 
 	public Object deserializeObject(String specifier, Class<?> desiredType, ClassLoader loader) {
+		if(ClassStringClassMapper.namedClasses.containsKey(specifier))
+			return ClassStringClassMapper.namedClasses.get(specifier);
 		if(loader == null)
 			loader = ClassStringClassMapper.class.getClassLoader();
 		try {
